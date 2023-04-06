9807f0378cec2fd6220afa468adb647fc91d6bd1
pianti10 <nicolaspiantini@gmail.com>
2023-04-05 22:03:41 -0300

    Deploy front

--- client/src/components/Navbar/SearchBar.jsx
+++ client/src/components/Navbar/SearchBar.jsx
@@ -20,7 +20,7 @@ export default function SearchBar() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    if (window.location.href !== "https://bolsillofeliz.vercel.app/home") {
+    if (window.location.href !== "https://bolsillo-feliz.vercel.app/home") {
       getCategoryParams(dispatch, brand, category, valor, pname)
         .then(navigate("/home"))
         .then(() => getCategoryParams(dispatch, brand, category, valor, pname));
--- client/src/redux/axios.js
+++ client/src/redux/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-    baseURL:  "bolsillo-feliz-production.up.railway.app"
+    baseURL:  "https://bolsillo-feliz-production.up.railway.app"
 });
 
 export default instance;
